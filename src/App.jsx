a44e311a4f72f135f7df9a96f828c2d80a80fcc6
@LuisArana <disconluis@gmail.com>
2022-02-08 18:11:42 +0100

    fix: Fix styles of form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useRef } from "react";
 import "./App.css";
+import { useField } from "./hooks/useField";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
   const avatarImg = useRef(null);
-  const handleClick = () => {
+  const [avatar, setAvatar] = useLocalStorage(
+    "avatar",
+    "./images/no-avatar.png"
+  );
+
+  const handleClick = (e) => {
+    e.preventDefault();
     const nroRandom = Math.floor(Math.random() * 1000);
-    const avatar = `https://avatars.dicebear.com/api/avataaars/${nroRandom}.svg`;
-    avatarImg.current.src = avatar;
+    const avatarRandom = `https://avatars.dicebear.com/api/avataaars/${nroRandom}.svg`;
+    setAvatar(avatarRandom);
   };
+
+  const handleNext = (params) => {
+    console.log(params);
+  };
+
+  const username = useField({ type: "text" });
   return (
     <main>
       <section>
@@ -16,11 +30,12 @@ function App() {
           <form action="#">
             <div className="form-group">
               <label htmlFor="email">Correo:</label>
-              <input type="email" name="email" id="email" />
+              <input {...username} name="email" id="email" required />
             </div>
-            <div className="form-group"></div>
-            <label htmlFor="name">Nombre completo:</label>
-            <input type="text" id="name" />
+            <div className="form-group">
+              <label htmlFor="name">Nombre completo:</label>
+              <input type="text" name="name" id="name" required />
+            </div>
 
             <div className="form-group">
               <label htmlFor="description">Descripción</label>
@@ -35,26 +50,27 @@ function App() {
             <div className="form-group flex-container">
               <div className="flex-1">
                 <label htmlFor="country">Pais:</label>
-                <input type="text" />
+                <input type="text" id="country" name="country" required />
               </div>
               <div className="flex-1">
                 <label htmlFor="city">Ciudad de residencia</label>
-                <input type="text" />
+                <input type="text" id="city" name="city" required />
               </div>
             </div>
             <div className="form-group flex-container">
               <div className="avatar flex-1">
-                <img
-                  ref={avatarImg}
-                  src="./images/no-avatar.png"
-                  alt="Avatar"
-                />
+                <img ref={avatarImg} src={avatar} alt="Avatar" />
               </div>
               <div className="button flex-1">
-                <button className="btn" onClick={handleClick}>
+                <button className="btn bg-secondary" onClick={handleClick}>
                   Generar
                 </button>
               </div>
+            </div>
+            <div className="form-group">
+              <button className="btn bg-primary" onClick={handleNext}>
+                Siguiente
+              </button>
             </div>
           </form>
         </div>
